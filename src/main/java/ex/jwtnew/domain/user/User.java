fdf85730a8c7d5fe42987e587129816a0e3d761b
yunjae62 <wodbs7758@naver.com>
2025-02-23 00:33:23 +0900

    feat: User 엔티티 추가

---
+++ src/main/java/ex/jwtnew/domain/user/User.java
@@ -0,0 +1,29 @@
+package ex.jwtnew.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity
+@Table(name = "users")
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String password;
+
+    public User(String username, String password) {
+        this.username = username;
+        this.password = password;
+    }
+}
