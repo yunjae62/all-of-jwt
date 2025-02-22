da00c9b279e8efd95cfc6ef5dd6d70cf512281fa
yunjae62 <wodbs7758@naver.com>
2025-02-23 00:33:07 +0900

    first commit

---
+++ src/main/java/ex/jwtnew/JwtNewApplication.java
@@ -0,0 +1,12 @@
+package ex.jwtnew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtNewApplication.class, args);
+    }
+}
---
+++ src/test/java/ex/jwtnew/JwtNewApplicationTests.java
@@ -0,0 +1,12 @@
+package ex.jwtnew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
