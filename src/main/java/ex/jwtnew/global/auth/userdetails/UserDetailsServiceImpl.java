28a285ba678e1ceef68c2d23bd729bf81b022da3
yunjae62 <wodbs7758@naver.com>
2025-02-25 20:30:24 +0900

    feat: UserDetailsService 추가

---
+++ src/main/java/ex/jwtnew/global/auth/userdetails/UserDetailsServiceImpl.java
@@ -0,0 +1,26 @@
+package ex.jwtnew.global.auth.userdetails;
+
+import ex.jwtnew.domain.user.User;
+import ex.jwtnew.domain.user.UserRepository;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Slf4j
+@Service
+@RequiredArgsConstructor
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = userRepository.findByUsername(username)
+            .orElseThrow(() -> new UsernameNotFoundException("Not Found " + username));
+
+        return new UserDetailsImpl(user.getUsername(), user.getPassword(), user.getRole());
+    }
+}
