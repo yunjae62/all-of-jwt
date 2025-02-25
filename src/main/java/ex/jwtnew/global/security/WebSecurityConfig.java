c3f1441390b74b9f4eefe3074b5b4a4116896491
yunjae62 <wodbs7758@naver.com>
2025-02-25 20:34:57 +0900

    feat: 시큐리티 설정 객체 추가

--- src/main/java/ex/jwtnew/global/security/WebSecurityConfig.java
+++ src/main/java/ex/jwtnew/global/security/WebSecurityConfig.java
@@ -1,9 +1,16 @@
 package ex.jwtnew.global.security;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import ex.jwtnew.global.auth.filter.AuthFilter;
+import ex.jwtnew.global.auth.filter.LoginFilter;
+import ex.jwtnew.global.auth.jwt.JwtUtil;
+import lombok.RequiredArgsConstructor;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
@@ -11,10 +18,14 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 @Configuration
 @EnableWebSecurity
+@RequiredArgsConstructor
 public class WebSecurityConfig {
+
+    private final JwtUtil jwtUtil;
 
     @Bean
     public PasswordEncoder passwordEncoder() {
@@ -22,15 +33,39 @@ public class WebSecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+    public AuthenticationManager authenticationManager(AuthenticationConfiguration configuration) throws Exception {
+        return configuration.getAuthenticationManager();
+    }
+
+    @Bean
+    public AuthFilter jwtAuthFilter(AuthenticationManager authenticationManager) {
+        return new AuthFilter(jwtUtil, authenticationManager);
+    }
+
+    @Bean
+    public LoginFilter loginFilter(AuthenticationManager authenticationManager, ObjectMapper objectMapper) {
+        LoginFilter filter = new LoginFilter(jwtUtil, objectMapper);
+        filter.setAuthenticationManager(authenticationManager);
+        return filter;
+    }
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(
+        HttpSecurity http,
+        LoginFilter loginFilter,
+        AuthFilter authFilter
+    ) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable);
 
         http.sessionManagement(manager -> manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
+        http.addFilterBefore(authFilter, LoginFilter.class);
+        http.addFilterBefore(loginFilter, UsernamePasswordAuthenticationFilter.class);
+
         http.authorizeHttpRequests(authz -> authz
             .requestMatchers(PathRequest.toStaticResources().atCommonLocations()).permitAll()
-            .requestMatchers(HttpMethod.POST, "/users/**").permitAll()
+            .requestMatchers(HttpMethod.POST, "/users/signup").permitAll()
             .anyRequest().authenticated()
         );
 
