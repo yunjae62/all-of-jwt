c0251c13bf1c36c3ca9188f5c0b7ba30c2e10d26
yunjae62 <wodbs7758@naver.com>
2025-02-26 14:59:59 +0900

    feat: 액세스토큰, 리프레쉬토큰 인증 객체 추가

---
+++ src/main/java/ex/jwtnew/global/auth/authentication/AccessTokenAuthentication.java
@@ -0,0 +1,29 @@
+package ex.jwtnew.global.auth.authentication;
+
+import java.util.Collection;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@ToString(callSuper = true)
+public class AccessTokenAuthentication extends JwtAuthentication {
+
+    /**
+     * 인증 전
+     */
+    public AccessTokenAuthentication(String token) {
+        super(token);
+    }
+    
+    /**
+     * 인증 후
+     */
+    public AccessTokenAuthentication(
+        Object principal,
+        String token,
+        Collection<? extends GrantedAuthority> authorities
+    ) {
+        super(principal, token, authorities);
+    }
+}
---
+++ src/main/java/ex/jwtnew/global/auth/authentication/RefreshTokenAuthentication.java
@@ -0,0 +1,29 @@
+package ex.jwtnew.global.auth.authentication;
+
+import java.util.Collection;
+import lombok.Getter;
+import lombok.ToString;
+import org.springframework.security.core.GrantedAuthority;
+
+@Getter
+@ToString(callSuper = true)
+public class RefreshTokenAuthentication extends JwtAuthentication {
+
+    /**
+     * 인증 전
+     */
+    public RefreshTokenAuthentication(String token) {
+        super(token);
+    }
+
+    /**
+     * 인증 후
+     */
+    public RefreshTokenAuthentication(
+        Object principal,
+        String token,
+        Collection<? extends GrantedAuthority> authorities
+    ) {
+        super(principal, token, authorities);
+    }
+}
