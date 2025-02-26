1adb43f1a48f697b08a722b3a809b08f5ae78fe6
yunjae62 <wodbs7758@naver.com>
2025-02-26 16:25:50 +0900

    feat: 테스트용 인증 필요 API 추가

--- src/main/java/ex/jwtnew/domain/user/UserController.java
+++ src/main/java/ex/jwtnew/domain/user/UserController.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -32,5 +33,10 @@ public class UserController {
         User savedUser = userRepository.save(user);
 
         return ResponseEntity.ok(savedUser);
+    }
+
+    @GetMapping("/test")
+    public ResponseEntity<Void> test() {
+        return ResponseEntity.ok().build();
     }
 }
