ffa825304ae94cada0bb4baab96d97d373f6583f
yunjae62 <wodbs7758@naver.com>
2025-02-26 13:47:46 +0900

    refactor: Jwt 인증 공급자에서 인증 유효성 처리

--- src/main/java/ex/jwtnew/global/auth/provider/JwtAuthenticationProvider.java
+++ src/main/java/ex/jwtnew/global/auth/provider/JwtAuthenticationProvider.java
@@ -1,15 +1,18 @@
 package ex.jwtnew.global.auth.provider;
 
 import ex.jwtnew.global.auth.authentication.JwtAuthentication;
+import ex.jwtnew.global.auth.jwt.JwtStatus;
 import ex.jwtnew.global.auth.jwt.JwtUtil;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.stereotype.Component;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j(topic = "JwtAuthenticationProvider")
 @Component
@@ -21,11 +24,30 @@ public class JwtAuthenticationProvider implements AuthenticationProvider {
 
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        String token = (String) authentication.getCredentials();
+        String tokenWithBearer = (String) authentication.getCredentials();
+        String token = jwtUtil.getTokenWithoutBearer(tokenWithBearer);
+
+        // 액세스 토큰 유효성 검사
+        JwtStatus tokenStatus = jwtUtil.validateToken(token);
+        validateTokenStatus(tokenStatus);
+
         String username = jwtUtil.getUsernameFromToken(token);
         UserDetails userDetails = userDetailsService.loadUserByUsername(username);
+        // 로그아웃 여부 확인 후 예외처리
 
         return new JwtAuthentication(username, token, userDetails.getAuthorities());
+    }
+
+    private void validateTokenStatus(JwtStatus tokenStatus) {
+        if (tokenStatus.equals(JwtStatus.INVALID)) {
+            // 무효한 토큰 에러 응답
+            throw new HttpClientErrorException(HttpStatusCode.valueOf(400));
+        }
+
+        if (tokenStatus.equals(JwtStatus.EXPIRED)) {
+            // 리프레쉬 하라는 에러 응답
+            throw new HttpClientErrorException(HttpStatusCode.valueOf(401));
+        }
     }
 
     @Override
