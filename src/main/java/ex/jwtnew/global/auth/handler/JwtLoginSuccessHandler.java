7ac60a10f692b3d875d01c517e7aac5c8fb21822
yunjae62 <wodbs7758@naver.com>
2025-02-26 13:14:55 +0900

    feat: 로그인 성공 핸들러를 별도의 객체로 추가

---
+++ src/main/java/ex/jwtnew/global/auth/handler/JwtLoginSuccessHandler.java
@@ -0,0 +1,54 @@
+package ex.jwtnew.global.auth.handler;
+
+import ex.jwtnew.global.auth.jwt.JwtUtil;
+import ex.jwtnew.global.auth.userdetails.UserDetailsImpl;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.MediaType;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.web.authentication.AuthenticationSuccessHandler;
+import org.springframework.stereotype.Component;
+
+@Slf4j
+@Component
+@RequiredArgsConstructor
+public class JwtLoginSuccessHandler implements AuthenticationSuccessHandler {
+
+    private final JwtUtil jwtUtil;
+
+    @Override
+    public void onAuthenticationSuccess(
+        HttpServletRequest request,
+        HttpServletResponse response,
+        Authentication authentication
+    ) throws IOException {
+        log.info("success auth loginId : {}", authentication.getName());
+
+        getResponseDtoWithTokensInHeader(authentication, response); // 헤더에서 토큰을 추가한 응답 DTO 생성
+
+        response.setContentType(MediaType.APPLICATION_JSON_VALUE); // JSON 설정
+        response.setCharacterEncoding(StandardCharsets.UTF_8.name()); // UTF8 설정하여 한글 표시
+
+//        String result = objectMapper.writeValueAsString(res); // JSON to String 변환
+        String result = "";
+        response.getWriter().write(result);
+    }
+
+
+    private void getResponseDtoWithTokensInHeader(Authentication authentication, HttpServletResponse response) {
+        UserDetailsImpl principal = (UserDetailsImpl) authentication.getPrincipal();
+        String username = principal.getUsername();
+        String role = List.of(principal.getAuthorities()).getFirst().toString();
+
+        String accessToken = jwtUtil.createAccessToken(username, role);
+        String refreshToken = jwtUtil.createRefreshToken(username, role);
+
+        response.addHeader(JwtUtil.ACCESS_TOKEN_HEADER, jwtUtil.setTokenWithBearer(accessToken));
+        response.addHeader(JwtUtil.REFRESH_TOKEN_HEADER, jwtUtil.setTokenWithBearer(refreshToken));
+    }
+}
