f99b7a21e5c7a8b65274d0a60ce0098b9def5f8a
yunjae62 <wodbs7758@naver.com>
2025-02-27 16:25:49 +0900

    feat: 공통 예외 객체 추가

---
+++ src/main/java/ex/jwtnew/global/exception/GlobalException.java
@@ -0,0 +1,18 @@
+package ex.jwtnew.global.exception;
+
+import ex.jwtnew.global.response.ErrorCase;
+import lombok.Getter;
+
+/**
+ * 전역 예외 처리를 위한 커스텀 예외 클래스 - ResultCase 받아서 처리
+ */
+@Getter
+public class GlobalException extends RuntimeException {
+
+    private final ErrorCase errorCase;
+
+    public GlobalException(ErrorCase errorCase) {
+        super(errorCase.getMessage());
+        this.errorCase = errorCase;
+    }
+}
